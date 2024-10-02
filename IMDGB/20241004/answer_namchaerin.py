674f0007a8b4491da8849272ab7fa1fcc8de1496
olive-f <oliveinthefores@gmail.com>
2024-10-02 10:23:27 +0900

    test

---
+++ IMDGB/20241004/answer_namchaerin.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import yfinance as yf
+# pip install yfinance --upgrade --no-cache-dir <- 이거 bash terminal 에서 실행
+from pandas import DataFrame
+# import plotly
+
+# 문제 1. 
+"""
+개발환경 설정 및, 깃허브 push 전과정 서술
+"""
+답1 = "ㅇㅇㅇㅇㅇㅇㅇㅇ"
+print(답1)
+
+# 문제 2. 
+
+# 내보내기 문제 : 이미지 2개, dataframe 내보내기 n개
+# 내보내기 경로는 output으로 잡으면 됨.
+# 
+text = [1, 2, 3]
+print(text) 
