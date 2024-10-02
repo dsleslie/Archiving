32b3a6b323cca9cbd65b5c027941a63edbd879ff
olive-f <oliveinthefores@gmail.com>
2024-10-02 11:25:38 +0900

    11:25

--- IMDGB/step01_python/242002_lecture_pandas.py
+++ IMDGB/step01_python/242002_lecture_pandas.py
@@ -10,7 +10,16 @@ data = [
 columns = ["종목코드", "종목명", "현재가"]
 df = DataFrame(data=data, columns=columns)
 df.set_index("종목코드", inplace=True)
-print(df)
+# print(df)
 
 
 # 정렬 하기
+# df.sort_values(by='현재가')
+# print(df.sort_values(by='현재가'))
+# ascending=False : 내림차순 , = True 가 기본 값= 오름차순
+print(df.sort_values(by='현재가',ascending=False))
+print(df.sort_values(by='종목명',ascending=False))
+
+print(df.sort_index()) # 기본값
+print(df.sort_index(ascending=False)) # 역순
+
