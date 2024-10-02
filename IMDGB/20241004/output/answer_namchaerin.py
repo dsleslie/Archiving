2657e7c69c9b75adf28e406b57770de547a1d106
olive-f <oliveinthefores@gmail.com>
2024-10-02 11:16:02 +0900

    test

--- IMDGB/20241004/output/answer_namchaerin.py
+++ IMDGB/20241004/output/answer_namchaerin.py
@@ -1,3 +1,4 @@
+#%%
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -12,7 +13,7 @@ from pandas import DataFrame
 개발환경 설정 및, 깃허브 push 전과정 서술
 """
 답1 = "ㅇㅇㅇㅇㅇㅇㅇㅇ"
-print(답1)
+#print(답1)
 
 # 문제 2. 
 
@@ -20,6 +21,26 @@ print(답1)
 # 내보내기 경로는 output으로 잡으면 됨.
 # 
 text = [1, 2, 3]
-print(text) 
+#print(text) 
 
 # 중요하다고 한 기초 문법 나온다. 시각화는 하나는 그대로, 하나는 응용해서 나옴.
+
+
+# estate=pd.read_csv("c:/Archiving/IMDGB/step02_ds_basic/dataset/seoul_real_estate.csv")
+
+apple = yf.download("AAPL", start = "2020-01-01", end="2024-09-30")
+nvidia = yf.download("NVDA", start = "2020-01-01", end="2024-09-30")
+intel = yf.download("INTC", start="2020-01-01", end = "2024-09-30")
+amazon = yf.download("AMZN", start="2020-01-01", end = "2024-09-30")
+
+# 그래프 1
+fig, ax = plt.subplots()
+ax.plot(apple['Open'], label = 'apple')
+ax.plot(nvidia['Open'], label = 'nvidia')
+ax.set_title('Stocks')
+ax.legend()
+# %% 
+plt.show()
+plt.savefig('/output/myStocks111.png', format='png')
+
+
