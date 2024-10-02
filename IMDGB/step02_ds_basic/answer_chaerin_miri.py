b08dfb9abb08f02896de13ded7f7d3537aa70560
olive-f <oliveinthefores@gmail.com>
2024-10-02 10:12:09 +0900

    mock test

---
+++ IMDGB/step02_ds_basic/answer_chaerin_miri.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import yfinance as yf
+# pip install yfinance --upgrade --no-cache-dir <- 이거 bash terminal 에서 실행
+from pandas import DataFrame
+
+
+#df1=pd.read_excel("/dataset/ss_ex_1.xlsx")
+#print(df1)
+
+#문제1="이렇게 답을 작성하면 된다."
+#print(문제1)
+
+#df2=pd.read_csv("c:/Archiving/IMDGB/step02_ds_basic/dataset/seoul_real_estate.csv")
+#print(df2.head())
+
+#df3=pd.read_excel("c:/Archiving/IMDGB/step02_ds_basic/dataset/ss_ex_1.xlsx", parse_dates=['일자'], index_col=0)
+#print(df3.head())
+#print(type(df3.index))
+
+
+
+
