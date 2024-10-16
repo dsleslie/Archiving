e654b506b38bc96bad23217c22620212ee007fe1
olive-f <oliveinthefores@gmail.com>
2024-10-16 15:59:28 +0900

    15:59

--- IMDGB/step03_streamlit/streamlit_07.py
+++ IMDGB/step03_streamlit/streamlit_07.py
@@ -21,19 +21,20 @@ def main():
     # iris2 = iris.loc[iris['species']== val, :].reset_index(drop=True): 이거 아래 보면 그냥 한번에 dataframe에 넣엇다는 거임
     st.dataframe(iris.loc[iris['species']==val, :].reset_index(drop=True))
 
-    st.title("필터링")
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.markdown("### 필터링")
     number = st.number_input("Insert a 'Petal_Width' number")
     st.write("The current number is ", number)
     st.dataframe(iris.loc[iris['petal_width']>=number, :].reset_index(drop=True))
 
 
-    """
+    
     st.markdown("<hr>", unsafe_allow_html=True)
     st.markdown("## MultiSelect")
     cols = st.multiselect("복수의 컬럼을 선택하세요", iris.columns)
     st.write("선택된 컬럼 : ", cols)
     st.dataframe(iris.loc[:, cols])
-    """
+    
 
 if __name__ == '__main__':
     main()
