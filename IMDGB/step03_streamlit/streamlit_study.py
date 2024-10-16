f4ccc08491ea64f6207cb8a717cc3ab390872b73
olive-f <oliveinthefores@gmail.com>
2024-10-16 16:41:05 +0900

    16:41

--- IMDGB/step03_streamlit/streamlit_study.py
+++ IMDGB/step03_streamlit/streamlit_study.py
@@ -7,7 +7,7 @@ def main():
     st.title("API도 되나?")
 
     SERVICE_KEY = 'QN54WqiBTYKQUzG7FaY62TgZxUdM9KMZYwkjIAelmD062qWigUT4Tlj%2B%2BNoqgaLcvdqvqNvfZqP92%2Flwn5gB5A%3D%3D'
-    loanYM=202307
+    loanYM=201507
     numOfRows=10
     pageNo=1
     dataType="json"
@@ -21,7 +21,6 @@ def main():
 
     url = f'http://apis.data.go.kr/B551408/rent-loan-rate-multi-dimensional-info/dimensional-list?serviceKey={SERVICE_KEY}&loanYm={loanYM}&numOfRows={numOfRows}&pageNo={pageNo}&dataType={dataType}&cbGrd={cbGrd}&houseTycd={houseTycd}&jobCd={jobCd}&age={age}&income={income}&debt={debt}'
     req = requests.get(url)
-    print(url)
     content = req.json()
     df=pd.DataFrame(content)
 
